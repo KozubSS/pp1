f3551d7b1dab1bd5e186616d211a4554403b40d8
s-116-25 <s-116-25@gumiforest.ae.krakow.pl>
2020-01-11 12:22:07 +0100

    gbp

--- 08-DataStructures/zad11.py
+++ 08-DataStructures/zad11.py
@@ -4,7 +4,7 @@ print('Data\t\t Kurs')
 print('----------------------------')
 
 for k in GBP['rates']:
-    print(["effectiveData"],'\t',['mid'])
+    print(f'k['effectiveDate']:10,'\t',k['mid'])
     
     
     
